898066c2bebfab7f8265ccf38c2eb64a3f7ce4df
bhwon718 <bhwon718@gmail.com>
2023-05-02 16:32:06 +0900

    53:00

--- src/xunit/TestCaseTest.java
+++ src/xunit/TestCaseTest.java
@@ -1,19 +1,24 @@
 package xunit;
 
-public class TestCaseTest extends TestCase{
+public class TestCaseTest extends TestCase {
     public TestCaseTest(String name) {
         super(name);
     }
 
-    public void testRunning(){
-        WasRun wasRun = new WasRun("testMethod");
+    WasRun wasRun;
+
+    @Override
+    public void setUp() {
+        wasRun = new WasRun("testMethod");
+    }
+
+    public void testRunning() {
         Assert.assertEquals(false, wasRun.wasRun);
         wasRun.run();
         Assert.assertEquals(true, wasRun.wasRun);
     }
 
-    public void testSetUp(){
-        WasRun wasRun = new WasRun("testMethod");
+    public void testSetUp() {
         Assert.assertEquals(false, wasRun.wasSetUp);
         wasRun.run();
         Assert.assertEquals(true, wasRun.wasSetUp);
