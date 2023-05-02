931909da52a4c5bfb2f4bf239f986cbb0c366e71
bhwon718 <bhwon718@gmail.com>
2023-05-02 16:20:15 +0900

    51:45

--- src/xunit/WasRun.java
+++ src/xunit/WasRun.java
@@ -6,6 +6,12 @@ import java.lang.reflect.Method;
 public class WasRun extends TestCase {
 
     public boolean wasRun;
+    public boolean wasSetUp;
+
+    @Override
+    public void setUp() {
+        wasSetUp = true;
+    }
 
     public WasRun(String name) {
         super(name);
